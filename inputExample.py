c58a9909e1a8fb128c111b59a5f933ed21b21e01
AkhilPrabananth <akhil.throw.away@gmail.com>
2024-10-02 14:51:56 +0530

    Updated examples

--- inputExample.py
+++ inputExample.py
@@ -1,10 +1,10 @@
 import requests
 
 data = {
-    "heart_rate": 200,
+    "heart_rate": 20,
     "spo2": 91,
     "timestamp": "2024-10-02T09:00:06"
 }
 
-response = requests.post("http://127.0.0.1:8000/update/", json=data)
+response = requests.post("https://wearable-server.onrender.com/update/", json=data)
 print(response.json())
--- outputExample.py
+++ outputExample.py
@@ -1,6 +1,6 @@
 import requests
 
-latest_response = requests.get("http://127.0.0.1:8000/latest/")
+latest_response = requests.get("https://wearable-server.onrender.com/latest/")
 
 # Check if the retrieval was successful
 if latest_response.status_code == 200:
